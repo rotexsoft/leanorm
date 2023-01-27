3658ec12a21e7b484f78f54f205188ebe2f04dc8
Rotimi Ade <rotexdegba007-github@yahoo.ca>
2023-01-27 16:24:01 -0700

    Added CachingModel

--- src/LeanOrm/CachingModel.php
+++ src/LeanOrm/CachingModel.php
@@ -12,29 +12,30 @@ namespace LeanOrm;
 class CachingModel extends Model {
 
     protected static $cachedFetchedTableListFromDB = [];
+
     protected static $cachedFetchedTableColsFromDB = [];
-    
+
     protected function fetchTableListFromDB(): array {
-                
+
         if(array_key_exists($this->db_connector->getConnectionName(), static::$cachedFetchedTableListFromDB)) {
-            
+
             return static::$cachedFetchedTableListFromDB[$this->db_connector->getConnectionName()];
         }
-        
+
         static::$cachedFetchedTableListFromDB[$this->db_connector->getConnectionName()] = parent::fetchTableListFromDB();
-        
+
         return static::$cachedFetchedTableListFromDB[$this->db_connector->getConnectionName()];
     }
-    
+
     protected function fetchTableColsFromDB(string $table_name): array {
-        
+
         if(array_key_exists($table_name, static::$cachedFetchedTableColsFromDB)) {
-            
+
             return static::$cachedFetchedTableColsFromDB[$table_name];
         }
-        
+
         static::$cachedFetchedTableColsFromDB[$table_name] = parent::fetchTableColsFromDB($table_name);
-        
+
         return static::$cachedFetchedTableColsFromDB[$table_name];
     }
 }
