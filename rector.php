cf740defe9b948bd7dfcdb34fb2a911307bd3e9b
rotimi <rotimi@localhost>
2023-09-05 15:58:41 -0600

    Upgraded to rector 0.18

--- rector.php
+++ rector.php
@@ -28,17 +28,16 @@ return static function (RectorConfig $rectorConfigurator): void {
     $rectorConfigurator->import(SetList::CODE_QUALITY);
     $rectorConfigurator->import(SetList::CODING_STYLE);
     $rectorConfigurator->import(SetList::DEAD_CODE);
-    $rectorConfigurator->import(SetList::PSR_4);
     $rectorConfigurator->import(SetList::TYPE_DECLARATION);
     
-    // get services (needed for register a single rule)
-    $services = $rectorConfigurator->services();
-    $services->remove(\Rector\CodeQuality\Rector\If_\ShortenElseIfRector::class);
-    $services->remove(\Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector::class);
-    $services->remove(\Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector::class);
-    $services->remove(\Rector\CodingStyle\Rector\ClassMethod\UnSpreadOperatorRector::class);
-    $services->remove(\Rector\DeadCode\Rector\If_\RemoveDeadInstanceOfRector::class);
+    $skipables = [
+        \Rector\CodeQuality\Rector\If_\ShortenElseIfRector::class,
+        \Rector\CodingStyle\Rector\Catch_\CatchExceptionNameMatchingTypeRector::class,
+        \Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector::class,
+        \Rector\CodingStyle\Rector\ClassMethod\UnSpreadOperatorRector::class,
+        \Rector\DeadCode\Rector\PropertyProperty\RemoveNullPropertyInitializationRector::class,
+        \Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector::class,
+    ];
     
-    //TODO:PHP8 comment once PHP 8 becomes minimum version
-    (PHP_MAJOR_VERSION < 8) && $services->remove(Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPromotedPropertyRector::class);
+    $rectorConfigurator->skip($skipables);
 };
