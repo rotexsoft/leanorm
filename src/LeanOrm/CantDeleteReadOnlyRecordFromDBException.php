56589dde3b2135f7286acbeceaabc0e76fd5369e
rotimi <rotimi@localhost>
2024-02-03 21:08:59 -0700

    Reorganizing Exceptions

--- src/LeanOrm/CantDeleteReadOnlyRecordFromDBException.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-namespace LeanOrm;
-    
-class CantDeleteReadOnlyRecordFromDBException extends \Exception{}
-
--- src/LeanOrm/KeyingFetchResultsByPrimaryKeyFailedException.php
+++
@@ -1,5 +0,0 @@
-<?php
-declare(strict_types=1);
-namespace LeanOrm;
-    
-class KeyingFetchResultsByPrimaryKeyFailedException extends \Exception{}
